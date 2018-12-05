923acb041fee038f496a115d45ac35295b8806a9
ktts-code <33462333+ktts-code@users.noreply.github.com>
2018-12-05 22:04:24 +0200

    Add files via upload
    images&currencies

---
+++ app/database/migrations/2018_12_05_195227_images.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Images extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('images', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('url',150);
+          $table->integer('product_id')->unsigned();
+
+          $table->timestamps();
+          $table->softDeletes();   // deleted_at
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
