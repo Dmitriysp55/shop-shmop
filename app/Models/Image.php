514752f5539a301dca805166f721beb1f39064e0
Alexandr <35295488+MorozAlexandr@users.noreply.github.com>
2018-12-08 00:11:52 +0200

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,7 +8,7 @@ class Image extends Model
 {
   protected $fillable = ['url','product_id'];
 
-  public function product(){
+  public function products(){
       return $this->belongsTo(\App\Models\Product::class);
     }
 }
