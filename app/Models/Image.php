5bf31584dca6a44816a454203197cfc3c8bff3e1
Alexandr <35295488+MorozAlexandr@users.noreply.github.com>
2018-12-07 23:00:40 +0200

    Add files via upload

---
+++ app/Models/Image.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+  protected $fillable = ['url','product_id'];
+
+  public function product(){
+      return $this->belongsToMany(\App\Models\Product::class);
+    }
+}
