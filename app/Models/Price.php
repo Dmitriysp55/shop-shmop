90f4d4b5086bb00a17a2e6b0c9ea0edfbd9f4c48
Alexandr <35295488+MorozAlexandr@users.noreply.github.com>
2018-12-07 22:56:05 +0200

    Add files via upload

---
+++ app/Models/Price.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Price extends Model
+{
+  protected $fillable = ['value','type','carrency_id','product_id'];
+
+  public function product(){
+      return $this->hasMany(\App\Models\Product::class);
+    }
+}
